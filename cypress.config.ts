da1fa1dff635070f829cfad6d636437c222e0d09
aabramowich <aabramowich@salesforce.com>
2025-05-30 18:20:49 -0300

    fix cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts,jsx,tsx}',
+    supportFile: 'cypress/support/e2e.ts',
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {
-
-    },
   },
 });
